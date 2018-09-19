0adc7b13a30ac8534f755d69292d969432558596
sekito charlse <bozicslxye1@gmail.com>
2018-09-19 10:49:03 +0200

    [feature #160419776] add register user functionality

---
+++ js/login.js
@@ -0,0 +1,27 @@
+function registeruser(e) {
+  e.preventDefault();
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  console.log(username, password);
+  data = {
+    'username': username,
+    'password': password
+  };
+  var url = "http://localhost:5000/api/v2/auth/register";
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json"
+    }
+  })
+  .then(res => res.json())
+  .then((response) => {
+               message = response
+               console.log(message['message'])
+               flash(message['message'],'danger')
+               
+  })
+  .catch(error => console.error("Error:", error));
+}

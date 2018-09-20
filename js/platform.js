d48d69effcff91d0278e86d708250ac57386f174
sekito charlse <bozicslxye1@gmail.com>
2018-09-20 12:28:59 +0200

    [feature #160419776] add home page for the web app

---
+++ js/platform.js
@@ -0,0 +1,90 @@
+ var url = "http://localhost:5000/api/v2/questions";
+ token = localStorage.getItem('token')
+
+ function flash(message,uiClass){
+    // add text
+    range = document.createRange();
+    div = document.createElement('div')
+    newNode = document.createElement('p')
+    newNode.className = uiClass
+    newNode.appendChild(document.createTextNode('Welcome back user'));
+    div.appendChild(newNode)
+    range.selectNode(document.getElementById('main_field')) 
+    range.insertNode(div)
+    //Time out
+    setTimeout(()=>{
+        const currentFlash = document.querySelector("."+uiClass);
+        if (currentFlash){
+            currentFlash.remove()
+        }else {
+            console.log('i am not working')
+        }
+    },1000)
+
+ }
+
+
+ fetch(url, {
+         method: "GET",
+         headers: {
+             Accept: "application/json, text/plain, */*",
+             "Content-Type": "application/json",
+             "Authorization": ` Bearer ${token}`
+         }
+     })
+     .then((res) => {
+         return res.json();
+     })
+     .then((response)=> {
+        flash('all questions','danger')
+        console.log(response)
+         questions = response.platform_questions
+
+         let ids = []
+         let title = []
+         let body = []
+         let authors = []
+
+         function get_data(questions) {
+             for (let i = 0; i < questions.length; i++) {
+                 title.push(questions[i]['title'])
+                 body.push(questions[i]['body'])
+                 ids.push(questions[i]['id'])
+                 authors.push(questions[i]['author'])
+             }
+
+         }
+         get_data(questions)
+         console.log(authors)
+
+         urls = []
+         let html = '<h2> Questions asked by other users </h2>'
+         let answers_html = ''
+         for (let i = 0; i < ids.length; i++) {
+             html += ` 
+              <a href="${url}" id=${ids[i]}>${title[i]}<a/> by <b>${authors[i]}</b> <br> 
+                                            `
+             urls.push(`http://localhost:5000/api/v2/questions/${ids[i]}`)
+             url = `http://localhost:5000/api/v2/questions/${ids[i]}`
+
+         }
+         console.log(urls)
+
+
+         specific_question = document.getElementById('questions')
+         specific_question.onclick = (e) => {
+             e.preventDefault()
+             console.log(e.target)
+             if (e.target && e.target.nodeName == 'A') {
+                 // console.log(e.target) 
+                 id = parseInt(e.target.attributes.getNamedItem('id').value);
+                 // qnid = e.target.attributes.getNamedItem('id').value;
+                 var url = `http://localhost:5000/api/v2/questions/${id}`;
+                 localStorage.setItem('id',id)
+                 window.location.href='answers.html'   
+                 
+             }
+         }
+
+         document.getElementById('questions').innerHTML = html;
+     });
